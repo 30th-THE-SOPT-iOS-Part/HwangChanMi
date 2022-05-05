2a171cb18180a187eadeaf1f6e7a449137d5e15c
mimi <gkfn980@hs.ac.kr>
2022-05-05 16:36:05 +0900

    🍱[ADD] :피드 에셋 추가 # 6

---
+++ instagramLogin/instagramLogin/Constants/Image.swift
@@ -0,0 +1,27 @@
+//
+//  Image.swift
+//  instagramLogin
+//
+//  Created by 황찬미 on 2022/05/03.
+//
+
+import UIKit
+
+extension Const {
+    public enum Image {
+        public static let instagram = UIImage(named: "instagram")
+        public static let instagramSmall = UIImage(named: "logo_instagram_small")
+
+        public static let avatar1 = UIImage(named: "avatar1")
+        public static let avatar2 = UIImage(named: "avatar2")
+        public static let avatar3 = UIImage(named: "avatar3")
+        public static let avatar4 = UIImage(named: "avatar4")
+        public static let avatar5 = UIImage(named: "avatar5")
+        public static let avatar6 = UIImage(named: "avatar6")
+        
+        public static let feedImg1 = UIImage(named: "postImage1")
+        public static let feedImg2 = UIImage(named: "postImage2")
+        public static let feedImg3 = UIImage(named: "postImage3")
+        public static let feedImg4 = UIImage(named: "postImage4")
+    }
+}
